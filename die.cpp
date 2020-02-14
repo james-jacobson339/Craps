1f208f60bcccab2459e27e1f0203e38315972dfc
James Jacobson <james.jacobson@snhu.edu>
2020-02-14 13:46:11 -0500

    End of Class

--- die.cpp
+++ die.cpp
@@ -3,3 +3,45 @@
 //
 
 #include "die.h"
+#include <random>
+
+Die (int startingNumberOfSides, int startingIncrement, int startingStartingValue, std::string color, bool isSymbols = false, std::string material = "Bone") {
+
+}
+    int Die :: roll() {
+
+    }
+    void Die :: setNumberOfSides(int newNumberOfSides) {
+
+    }
+    int Die :: getNumberOfSides() {
+
+    }
+    void Die :: setIncrement(int newIncrement) {
+
+    }
+    int Die :: getIncrement() {
+
+    }
+    void Die :: setValue(int newValue) {
+
+    }
+    int Die :: getValue() {
+
+    }
+    void Die :: setColor(std::string newColor) {
+
+    }
+    std::string Die :: getColor() {
+
+    }
+    bool Die :: isSymbol();
+    {
+
+    }
+    void Die :: setMaterial(std::string newMaterial) {
+
+    }
+    std::string Die :: getMaterial() {
+
+    }
--- die.h
+++ die.h
@@ -4,5 +4,32 @@
 
 #ifndef CRAPS_DIE_H
 #define CRAPS_DIE_H
+#include <string>
+#include <iostream
+
+class Die {
+private:
+    int numberOfSides;
+    int increment;
+    int value;
+    std::string color;
+    bool isSymbols;
+    std::string material;
+    int startingValue;
+public:
+    Die (int startingNumberOfSides, int startingIncrement, int startingStartingValue, std::string color, bool isSymbols = false, std::string material = "Bone");
+    int roll();
+    void setNumberOfSides(int newNumberOfSides);
+    int getNumberOfSides();
+    void setIncrement(int newIncrement);
+    int getIncrement();
+    void setValue(int newValue);
+    int getValue();
+    void setColor(std::string newColor);
+    std::string getColor();
+    bool isSymbol();
+    void setMaterial(std::string newMaterial);
+    std::string getMaterial();
+};
 
 #endif //CRAPS_DIE_H
