68bc54915da3641eb7f3c315c15b0bc18629f579
James Jacobson <james.jacobson@snhu.edu>
2020-02-14 13:14:43 -0500

    addition of die .cpp & .h

---
+++ die.cpp
@@ -0,0 +1,5 @@
+//
+// Created by User on 2/14/2020.
+//
+
+#include "die.h"
---
+++ die.h
@@ -0,0 +1,8 @@
+//
+// Created by User on 2/14/2020.
+//
+
+#ifndef CRAPS_DIE_H
+#define CRAPS_DIE_H
+
+#endif //CRAPS_DIE_H
